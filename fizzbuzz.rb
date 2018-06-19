f2b73683bac53c69c5a64ca599d2f330f45db4de
yayam15 <yazmyn.a.aguilar04@gmail.com>
2018-06-19 19:50:34 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,14 @@
 
 def fizz_buzz(int)
-elsif int%15 == 0
+  elsif int%15 == 0
     puts "fizzbuzz"
-end
   elsif int%3 == 0
     puts "fizz"
-end
+
   elsif int%5 == 0
     puts "buzz"
-end
+
   els
     puts "nil"
+ end
 end
