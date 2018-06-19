16388b6fb0eb41eabf9376669134d7303930f04c
yayam15 <yazmyn.a.aguilar04@gmail.com>
2018-06-19 19:29:50 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,17 @@
+def fizz_Buzz(3)
+  if int%3 == 0
+    puts "fizz"
+end
+  elsif int%5==0
+    puts "buzz"
+  elsif int%15==0
+    puts "fizzbuzz"
+  elsif
+    puts "nil"
+
+    /Users/apple/.atom/.learn-ide/home/yayam15/kwk-l1-ruby-2-meal-choice-lab-kwk-students-l1-bal-061818/README.md
+
+
+
+
+  c
